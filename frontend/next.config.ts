df615cca5cc4aa187b74fc01b7a1698cca80c284
umar khitab <umair123123ss@gmail.com>
2025-05-25 01:09:30 +0500

    fix: Update next.config.ts to include rewrites for API proxying

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+module.exports={
+  async rewrites(){
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*` // Proxy to Backend
+      }
+    ]
+  }
+}
+module.exports = nextConfig;
 
 export default nextConfig;
