c2af4acd261bbb044dcdb27efb9f439e3636ce1e
umar khitab <umair123123ss@gmail.com>
2025-05-30 16:13:11 +0500

    feat: add user prop to Navbar component in RootLayout for user-specific functionality

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-
+import {User} from '@/app/interfaces/user.interface'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,7 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <Navbar user={{
+        _id: '',
+        username: '',
+        email: '',
+        password: '',
+        avatar: '',
+
+      }} />
         {children}
       </body>
     </html>
