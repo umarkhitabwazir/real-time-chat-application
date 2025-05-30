d66477566c78ce2e169e076ec2d096a215ea3186
umar khitab <umair123123ss@gmail.com>
2025-05-30 16:04:26 +0500

    remove unuse useState

--- frontend/src/app/components/Profile.component.tsx
+++ frontend/src/app/components/Profile.component.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import { User } from '../interfaces/user.interface'
 
 const profileComponent:React.FC<User> = ({user}) => {
