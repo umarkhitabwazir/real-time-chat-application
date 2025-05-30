72fd137b8b969bff9808944cdeb25b0dca463608
umar khitab <umair123123ss@gmail.com>
2025-05-30 16:17:00 +0500

    fix: remove unused User import in layout.tsx for cleaner code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import {User} from '@/app/interfaces/user.interface'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
