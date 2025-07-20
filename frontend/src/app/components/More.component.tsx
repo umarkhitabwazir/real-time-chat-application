88841483170e1b2ee2f17b98a4480bb0e28226c0
umar khitab <umair123123ss@gmail.com>
2025-07-20 22:28:38 +0500

    fix: improve code formatting and readability in MoreComponent

--- frontend/src/app/components/More.component.tsx
+++ frontend/src/app/components/More.component.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation'
 
 
 
-const MoreComponent = ({currentUserName ,message,fetchMessages }: {currentUserName:string | null, message: Message ,fetchMessages:()=>Promise<void>}) => {
+const MoreComponent = ({ currentUserName, message, fetchMessages }: { currentUserName: string | null, message: Message, fetchMessages: () => Promise<void> }) => {
   const [showDelete, setShowDelete] = useState(false)
   const [showComponent, setShowComponent] = useState(false)
   const API = process.env.NEXT_PUBLIC_API_URL
@@ -14,10 +14,10 @@ const MoreComponent = ({currentUserName ,message,fetchMessages }: {currentUserNa
 
   const deleteForEveryOneHandler = async () => {
     try {
-       await axios.patch(`${API}/deleteForEveryOne/${message._id}`, {}, { withCredentials: true })
+      await axios.patch(`${API}/deleteForEveryOne/${message._id}`, {}, { withCredentials: true })
       await fetchMessages()
-          setShowDelete(true)
-          setShowDelete(false)
+      setShowDelete(true)
+      setShowDelete(false)
 
 
 
@@ -38,14 +38,14 @@ const MoreComponent = ({currentUserName ,message,fetchMessages }: {currentUserNa
   const deleteForMeHandler = async () => {
     try {
       await axios.patch(`${API}/deleteForMe/${message._id}`, {}, { withCredentials: true })
-     await fetchMessages()
-     setShowDelete(true)
-     setShowDelete(false)
-     
+      await fetchMessages()
+      setShowDelete(true)
+      setShowDelete(false)
+
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
         if (error.response?.data.error === "Unauthorized") {
-         setShowDelete(true)
+          setShowDelete(true)
 
 
           router.push(`/api/login?redirectTo=${encodeURIComponent(window.location.href)}`)
@@ -55,7 +55,11 @@ const MoreComponent = ({currentUserName ,message,fetchMessages }: {currentUserNa
   }
   return (
     <>
-      <div onMouseLeave={() => { setShowComponent(true), setShowDelete(false) }} className={`${showComponent ? "hidden" : ''} relative right-3 z-48  bg-blue-500 shadow-2xl rounded-md flex justify-center items-start p-1 h-60 w-40`}>
+      <div onMouseLeave={() => {
+        setShowComponent(true);
+          setShowDelete(false)
+      }} className={`${showComponent ? "hidden" : ''}
+         relative right-3 z-48  bg-blue-500 shadow-2xl rounded-md flex justify-center items-start p-1 h-60 w-40`}>
         <button
           onClick={() => setShowDelete((prev) => !prev)}
           className='text-white bg-green-500 w-full cursor-pointer hover:bg-green-400 rounded-md'>delete</button>
@@ -64,7 +68,7 @@ const MoreComponent = ({currentUserName ,message,fetchMessages }: {currentUserNa
             <button onClick={deleteForMeHandler} className='text-white bg-red-500 w-full cursor-pointer hover:bg-red-400 rounded-md '
             >delete for me </button>
             {
-              message.sender.username ===currentUserName && !message.deleteForEveryone &&
+              message.sender.username === currentUserName && !message.deleteForEveryone &&
               <button onClick={deleteForEveryOneHandler} className='text-white bg-red-500 w-full cursor-pointer hover:bg-red-400 rounded-md'
               >delete for everone </button>
             }
